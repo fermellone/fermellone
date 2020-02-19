b571bf79047a9fa036ee474f0fa945d49f705f41
Fernando Mellone <fernandomellone2@gmail.com>
2020-02-18 22:44:58 -0300

    se cambió el estilo de la vista contactos

--- src/views/Contacto.vue
+++ src/views/Contacto.vue
@@ -3,23 +3,23 @@
     <h2 class="text-center mt-4">MIS REDES SOCIALES</h2>
     <ul class="d-flex justify-content-between redes-sociales">
       <li class="red-social">
-        <a href="https://linked.in/fernando-mellone"><img src="@/assets/icons/linkedin-in-brands.svg" alt="Icono LinkedIn">LinkedIn</a>
+        <a href="https://linked.in/fernando-mellone"><img class="linkedin" src="@/assets/icons/linkedin-in-brands.svg" alt="Icono LinkedIn"></a>
       </li>
       <li class="red-social">
-        <a href="https://github.com/nandyme"><img src="@/assets/icons/github-brands.svg" alt="Icono GitHub"> GitHub</a>
+        <a href="https://github.com/nandyme"><img class="github" src="@/assets/icons/github-brands.svg" alt="Icono GitHub"></a>
       </li>
       <li class="red-social">
-        <a href="https://facebook.com/nandyme"><img src="@/assets/icons/facebook-brands.svg" alt="Icono Facebook"> Facebook</a>
+        <a href="https://facebook.com/nandyme"><img src="@/assets/icons/facebook-brands.svg" alt="Icono Facebook"></a>
       </li>
       <li class="red-social">
-        <a href="https://twitter.com/nandyme_"><img src="@/assets/icons/twitter-brands.svg" alt="Icono Twitter"> Twitter</a>
+        <a href="https://twitter.com/nandyme_"><img src="@/assets/icons/twitter-brands.svg" alt="Icono Twitter"></a>
       </li>
       <li class="red-social">
-        <a href="https://instagram.com/nandymexd"><img src="@/assets/icons/instagram-brands.svg" alt="Icono Instagram"> Instagram</a>
+        <a href="https://instagram.com/nandymexd"><img src="@/assets/icons/instagram-brands.svg" alt="Icono Instagram"></a>
       </li>
     </ul>
 
-    <h3 class="text-center">CONTÁCTAME DIRÉCTAMENTE</h3>
+    <h3 class="text-center directo-titulo">CONTÁCTAME DIRÉCTAMENTE</h3>
     <ul class="text-center datos-contacto">
       <li class="dato-contacto">Email: fernandomellone2@gmail.com</li>
       <li class="dato-contacto">Celular: +595 973 830182</li>
